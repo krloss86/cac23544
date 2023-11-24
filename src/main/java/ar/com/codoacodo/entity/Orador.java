09a789680ea20f112540a776a4fbefb223553f16
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-11-23 22:04:31 -0300

    implementacion repository

--- src/main/java/ar/com/codoacodo/entity/Orador.java
+++ src/main/java/ar/com/codoacodo/entity/Orador.java
@@ -97,6 +97,10 @@ public class Orador {
 		this.fechaAlta = fechaAlta;
 	}
 	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	///ALT+shit+s
 	
 }
---
+++ src/main/java/ar/com/codoacodo/interfaces/Interface.java
@@ -0,0 +1,11 @@
+package ar.com.codoacodo.interfaces;
+
+public interface Interface {
+	final String hola = "";
+	
+	public void metodo1();//definicion del metodo no tiene cuerpo
+	
+	public default void metodo2() {
+		System.out.println("Metodo2");
+	}
+}
