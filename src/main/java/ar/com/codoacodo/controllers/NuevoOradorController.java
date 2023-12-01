a5c31a97be7092dc916a867209731cd78f6f0058
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-11-30 22:33:49 -0300

    add cors conf

--- src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
+++ src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
@@ -12,20 +12,26 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+//http://localhost:8080/web-app-23544/api/orador/nuevo
 @WebServlet("/api/orador/nuevo")
 public class NuevoOradorController extends HttpServlet{
 
 	//crear > POST
-	protected void doGet(
+	protected void doPost(
 				HttpServletRequest request, //aca viene lo que manda el usuario 
 				HttpServletResponse response /*manda el backend al frontend*/
 			) throws ServletException, IOException {
 		
+		response.addHeader("Access-Control-Allow-Origin","*");
+		
 		//capturo los parametros enviados por el front
-		String nombre = request.getParameter("nombre");
+		String orador = request.getParameter("orador");//f6
+		String nombre = request.getParameter("nombre");//f6
 		String apellido = request.getParameter("apellido");
 		String email = request.getParameter("email");
 		String tema = request.getParameter("tema");
+		
+		//validar!!!
 		
 		//creo mi orador con esos paramtros
 		Orador nuevo = new Orador(nombre, apellido, email, tema, LocalDate.now());
@@ -36,7 +42,13 @@ public class NuevoOradorController extends HttpServlet{
 		repository.save(nuevo);
 		
 		//ahora respondo al front
-		response.getWriter().print("OK");//json
+		//response.getWriter().print("OK");//json
 	}
 
+	// Preflight
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.addHeader("Access-Control-Allow-Origin","*");
+	    response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, HEAD");
+	    response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+	}
 }
